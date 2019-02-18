d558982b9825f45b6d971613a1c12df18e32dde2
Michael Lev <michael.lev@automat-it.com>
2019-02-18 17:22:50 +0200

    Add webSocket tunnel location (proxy_pass) Nginx

--- udemy_complex/server/keys.js
+++ udemy_complex/server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
+  pgUser: process.env.POSTGRES_USER,
   pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
+  pgDatabase: process.env.POSTGRES_DB,
+  pgPassword: process.env.POSTGRES_PASSWORD,
   pgPort: process.env.PGPORT
 };
